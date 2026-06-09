6cd7497d3f2916c3512ceafa8a7462a4f0963221
zhouyaozy <2220389727@qq.com>
2026-06-09 08:39:43 +0000

    23

--- test/internal/constants.js
+++ test/internal/constants.js
@@ -6,7 +6,10 @@ const constants = require('../../internal/constants')
 t.match(constants, {
   MAX_LENGTH: Number,
   MAX_SAFE_COMPONENT_LENGTH: Number,
+  MAX_SAFE_BUILD_LENGTH: Number,
   MAX_SAFE_INTEGER: Number,
   RELEASE_TYPES: Array,
   SEMVER_SPEC_VERSION: String,
+  FLAG_INCLUDE_PRERELEASE: Number,
+  FLAG_LOOSE: Number,
 }, 'got appropriate data types exported')
